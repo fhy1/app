f646370f9e3e0af8fbd0bba72be8b64eedd7bef0
丰浩宇 <923978801@qq.com>
2019-11-01 15:37:19 +0800

    init

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import './app/Common/SetTheme'
+import './app/Common/Global'
+
+
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+console.ignoredYellowBox = ['Warning: BackAndroid is deprecated.  Please use BackHandler instead.',
+    'source.uri should not be an empty string','Remote debugger is in a background tab which',
+    'Setting a timer',
+    'Encountered two children with the same key,',
+    'Attempt to read an array index',
+];
+
+AppRegistry.registerComponent('ReactNativeTemplate', () => App);
