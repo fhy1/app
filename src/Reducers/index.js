0f5c5ac5a774aa5489bf35b4d6f89f5ad83f4d40
丰浩宇 <923978801@qq.com>
2019-11-05 16:59:35 +0800

    reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
+import home from './home';
 
-const rootReducers = combineReducers({});
+const rootReducers = combineReducers({home});
 
 export default rootReducers;
