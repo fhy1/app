750931eb0800916559b3df1502baeb7f70a30fb3
丰浩宇 <923978801@qq.com>
2019-11-06 17:08:09 +0800

    app

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import {View,Image,StyleSheet,Text} from 'react-native';
+import {View, Image, StyleSheet, Text, Dimensions} from 'react-native';
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      swiperHeight: 0,
+    };
+  }
+
+  componentWillMount() {
+    let {height, width} = Dimensions.get('window');
+    // console.warn(width);
+    this.setState({
+      // swiperHeight: width * (350 / 218),
+    });
+  }
+
   render() {
+    const {swiperHeight} = this.state;
     return (
       <View style={styles.homeView}>
-        <View style={styles.swiperView}>
-          <Image style={styles.swiperImage} source={require('../../assets/banner.png')} />
-        </View>
+        {/* <View style={{height: swiperHeight}}>
+          <Image
+            style={styles.swiperImage}
+            source={require('../../assets/banner.png')}
+          />
+        </View> */}
         <View></View>
         <View></View>
         {/* <Text>11111</Text> */}
@@ -27,8 +46,9 @@ const styles = StyleSheet.create({
     height: 217.5,
   },
   swiperImage: {
-    flex: 1
-  }
+    width: '100%',
+    height: '100%',
+  },
 });
 
 export default HomeScreen;
